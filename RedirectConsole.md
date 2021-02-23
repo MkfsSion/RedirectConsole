18b2878877de60533858550864f0b035db606774
MkfsSion <mkfssion@mkfssion.com>
2021-02-23 22:09:34 +0800

    RedirectConsole: Init project
    Signed-off-by: MkfsSion <mkfssion@mkfssion.com>

---
+++ RedirectConsole.md
@@ -0,0 +1,3 @@
+# RedirectConsole
+
+RedirectConsole is a simple tool for switching back to console session when connecting via RDP, which enables streaming using Moonlight.
---
+++ RedirectConsole/WTSSession.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+#include <cstdint>
+#include <memory>
+#include <vector>
+#include <WtsApi32.h>
+
+namespace WTSService {
+
+	struct SessionInfo
+	{
+		uint32_t sessionId;
+		std::string sessionName;
+		WTS_CONNECTSTATE_CLASS state;
+	};
+
+	class WTSSession {
+	private:
+		std::vector<SessionInfo> mSessions;
+	public:
+		WTSSession();
+		WTSSession(const WTSSession &session) = delete;
+		WTSSession(const WTSSession &&session) = delete;
+		~WTSSession() = default;
+		WTSSession &operator=(const WTSSession &session) = delete;
+		WTSSession &operator=(const WTSSession &&session) = delete;
+		const std::vector<SessionInfo> &getSessions();
+		const SessionInfo *getCurrentSession();
+		void connect(const SessionInfo &session);
+		void disconnect(const SessionInfo &session);
+		void disconnectCurrent();
+		inline size_t getSessionCount() {
+			return mSessions.size();
+		};
+	};
+}
